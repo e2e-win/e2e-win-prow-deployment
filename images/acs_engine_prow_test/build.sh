23ebf27692f78308ef056c9b650255977b63f74c
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2018-07-10 12:57:58 +0300

    Add trap to build script to push output if kubetest fails.

--- images/acs_engine_prow_test/build.sh
+++ images/acs_engine_prow_test/build.sh
@@ -15,6 +15,18 @@ AZ_DEPLOYMENT_NAME=prow-${PROW_JOB_ID}
 GS_BUCKET=${GS_BUCKET:-"gs://e2e-win-acs-engine"}
 GS_BUCKET_FULL_PATH=${GS_BUCKET}/${REPO_NAME}_${REPO_OWNER}/${PULL_NUMBER}/${JOB_NAME}/${PROW_JOB_ID}/${BUILD_NUMBER}
 
+function upload_results {
+
+    # Uploading results
+    echo "Uploading results"
+    gsutil cp -r ${OUTPUT_DIR} ${GS_BUCKET_FULL_PATH}
+
+}
+
+trap "upload_results" EXIT
+
+
+
 REPO=${REPO:-"http://github.com/Azure/acs-engine"}
 BRANCH=${BRANCH:-"master"}
 
@@ -90,9 +102,3 @@ echo "Running kubetest"
 # TO DO (atuvenie): hyperkube and zip should be passed as params
 
 ${KUBETEST} --deployment=acsengine --provider=azure --test=true --up=true --down=false --ginkgo-parallel=10 --acsengine-resource-name=${AZ_DEPLOYMENT_NAME} --acsengine-resourcegroup-name=${AZ_RG_NAME} --acsengine-admin-password=Passw0rdAdmin --acsengine-admin-username=azureuser --acsengine-orchestratorRelease=1.11 --acsengine-hyperkube-url=atuvenie/hyperkube-amd64:1011960828217266176 --acsengine-win-binaries-url=https://k8szipstorage.blob.core.windows.net/mystoragecontainer/1011960828217266176.zip --acsengine-creds=$AZURE_CREDENTIALS --acsengine-public-key=$AZURE_SSH_PUBLIC_KEY_FILE --acsengine-winZipBuildScript=$WIN_BUILD --acsengine-location=westus2 --test_args="--ginkgo.dryRun=false --ginkgo.focus=\\[Conformance\\]|\\[NodeConformance\\]"
-
-# Uploading results
-echo "Uploading results"
-
-gsutil cp -r ${OUTPUT_DIR} ${GS_BUCKET_FULL_PATH}
-
