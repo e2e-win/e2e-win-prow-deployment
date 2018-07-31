f000f4e045e883bb46941b1fdb5b83f32ab0b2c5
papagalu <dmititelu@cloudbasesolutions.com>
2018-07-31 12:32:46 +0300

    [images] added new image that updates the prow config file

---
+++ images/update-config/update-config.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -x
+set -e
+
+set -o pipefail
+
+function update_config () {
+    local config="$1"
+    kubectl create configmap $config --from-file=$config.yaml --dry-run -o yaml | kubectl replace configmap $config -f -
+}
+
+function validate () {
+    local config="$1"
+    git clone "https://github.com/e2e-win/test-infra"
+    pushd "test-infra"
+        bazel run //prow/cmd/config -- --config-path=$config
+    popd
+}
+
+function main () {
+    git clone "https://github.com/e2e-win/e2e-win-prow-deployment"
+    validate "/root/e2e-win-prow-deployment/prow-cluster/configmaps/config.yaml"
+    pushd "e2e-win-prow-deployment/prow-cluster/configmaps"
+        update_config "config"
+        update_config "plugins"
+    popd
+}
+
+main "$@"
