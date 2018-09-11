58d6e500bdea08794d3be384ff9d51cc9d14a8c3
Adelina Tuvenie <atuvenie@cloudbasesolutions.com>
2018-09-11 10:18:39 +0300

    change master extension

--- extensions/master_extension/win-e2e-master-extension.sh
+++ extensions/master_extension/win-e2e-master-extension.sh
@@ -3,3 +3,5 @@
 master_node=$(kubectl get nodes | grep master | awk '{print $1}')
 
 kubectl taint nodes $master_node key=value:NoSchedule
+kubectl label nodes $master_node node-role.kubernetes.io/master=NoSchedule
+
