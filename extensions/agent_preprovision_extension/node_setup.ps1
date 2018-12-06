61ef1fadf9ffba61689f668b5975f305f44002f9
Adelina Tuvenie <adelina-t@users.noreply.github.com>
2018-12-06 15:13:38 +0200

    Update node_setup.ps1

--- extensions/agent_preprovision_extension/node_setup.ps1
+++ extensions/agent_preprovision_extension/node_setup.ps1
@@ -1,4 +1,13 @@
 # Script file to create tmp directory in windows nodes
+# Also pulls images on the node
+
+Write-Host "Prepulling all test images"
+
+Start-BitsTransfer https://raw.githubusercontent.com/e2e-win/k8s_images/master/Utils.ps1
+Start-BitsTransfer https://raw.githubusercontent.com/e2e-win/k8s_images/master/PullImages.ps1
+
+./PullImages.ps1
+
 
 Write-Host "$(date)- Creating tmp directory"
 
